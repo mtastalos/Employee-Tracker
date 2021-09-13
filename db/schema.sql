d29f5d4cc490610a669c5be091b45e157d947701
Michael Astalos <michaeldont@gmail.com>
2021-09-12 23:32:35 -0400

    removed password

--- db/schema.sql
+++ db/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE employees (
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER NOT NULL,
   manager_id INTEGER,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
   CONSTRAINT uc_manager UNIQUE (manager_id),
   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
